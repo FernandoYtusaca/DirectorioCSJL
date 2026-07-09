af4919d416b1f4f30da9dd4d6f26faf820091f88
JulioCesarInga27 <151539955+JulioCesarInga27@users.noreply.github.com>
2026-07-09 12:09:21 -0500

    comint header
    Added onProfileClick method to HeaderComponent for profile interactions.

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  onProfileClick() {
+    console.log('Click en el perfil completo del administrador');
+    // Aquí podrás abrir un menú desplegable o ir a la configuración del perfil más adelante
+  }
+
+}
